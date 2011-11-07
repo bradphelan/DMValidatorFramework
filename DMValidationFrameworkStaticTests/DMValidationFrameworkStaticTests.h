71ad1ec419d52a85ec30744db4ec46c0b82f4ac8
Brad Phelan <bradphelan@xtargets.com>
2011-11-07 16:25:18 +0100

    Create universal static framework library
    using
    
    https://github.com/kstenerud/iOS-Universal-Framework

---
+++ DMValidationFrameworkStaticTests/DMValidationFrameworkStaticTests.h
@@ -0,0 +1,13 @@
+//
+//  DMValidationFrameworkStaticTests.h
+//  DMValidationFrameworkStaticTests
+//
+//  Created by brad phelan on 11/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DMValidationFrameworkStaticTests : SenTestCase
+
+@end
---
+++ DMValidationFrameworkStaticTests/DMValidationFrameworkStaticTests.m
@@ -0,0 +1,32 @@
+//
+//  DMValidationFrameworkStaticTests.m
+//  DMValidationFrameworkStaticTests
+//
+//  Created by brad phelan on 11/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DMValidationFrameworkStaticTests.h"
+
+@implementation DMValidationFrameworkStaticTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DMValidationFrameworkStaticTests");
+}
+
+@end
